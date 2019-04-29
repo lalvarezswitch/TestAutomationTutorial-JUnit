6440886e0828107537a5957a12cafd1f7a47f955
Luis Miguel Alvarez <lalvarez@switch.local>
2019-04-29 14:58:33 -0300

    first commit

---
+++ src/test/java/com/qualitystream/tutorial/GoogleSearchTest.java
@@ -0,0 +1,41 @@
+package com.qualitystream.tutorial;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+	private WebDriver driver;
+	
+	@BeforeEach
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+	
+	@Test
+	public void testGooglePage() {
+		WebElement searchbox = driver.findElement(By.name("q"));
+		searchbox.clear();
+		searchbox.sendKeys("quality-stream Introducción a la Automatización de Pruebas de Software");
+		searchbox.submit();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		assertEquals("quality-stream Introducción a la Automatización de Pruebas de Software - Buscar con Google", driver.getTitle());
+		
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		driver.quit();
+	}
+}
